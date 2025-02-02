2fe9e44925b8b5b351f53639ff753da2d5661a34
CountItLikeACoder <157337289+CountItLikeACoder@users.noreply.github.com>
2025-02-02 18:56:47 -0500

    Add files via upload

---
+++ Twitter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    char message[141]; // array that can hold 140 characters for the message
+
+    // prompt user to input their message
+    printf("Enter your message: ");
+    fgets(message, sizeof(message), stdin); // read message
+
+    // remove newline character
+    message[strcspn(message, "\n")] = '\0';
+
+    //check message length
+    if (strlen(message) <= 140) {
+        printf("Posted");
+    } else {
+        printf("Rejected");
+    }
+}
