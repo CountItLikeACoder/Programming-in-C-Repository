e3ab69b82ef5bab90c9a2fbacc93e3a15e9a1345
CountItLikeACoder <157337289+CountItLikeACoder@users.noreply.github.com>
2025-02-02 19:49:40 -0500

    Twitter.c

--- Twitter.c
+++ Twitter.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void main() {
-    char message[141]; // array that can hold 141 characters (140 characters + null terminator) for the message
+int main() {
+    char message[142]; // array that can hold 141 characters (140 characters + null terminator) for the message
 
     // prompt user to input their message
     printf("Enter your message: ");
@@ -10,11 +10,13 @@ void main() {
 
     // remove newline character
     message[strcspn(message, "\n")] = '\0';
-
+    
     //check message length
     if (strlen(message) <= 140) {
-        printf("Posted");
+        printf("Posted\n");
     } else {
-        printf("Rejected");
+        printf("Rejected\n");
     }
+
+    return 0;
 }
